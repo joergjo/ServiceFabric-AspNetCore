7829c3ab5dd5102fb3cf661de6cb826de60e912c
Joerg Jooss (MSFT) <joerg.jooss@microsoft.com>
2016-11-06 22:54:27 +0100

    Added ARM project to create SF cluster

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 Sample solution that demonstrates using [ASP.NET Core](https://www.asp.net/core) in 
 [Azure Service Fabric](https://azure.microsoft.com/en-us/services/service-fabric/).
 
++ SFApp: A Service Fabric application that contains both SimpleWebApp and WebListenerWebApp. 
+
 + SimpleWebApp: Shows an ASP.NET MVC Core app hosted in Kestrel using HTTPS running in Service Fabric. 
 Please make sure to specifiy your certificate's thumbprint in the SimpleWebApp's `Settings.xml`. You can use any 
 self-signed or valid certificate, but make sure to store it in your machine's `LocalMachine\My` store. 
@@ -11,9 +13,15 @@ If you have IIS Express installed, you can use its certificate instead of creati
 the Kestrel sample, WebListener requires an `<EndpointPolicy />` and an `<EndpointCertificate />` in the Application Manifest 
 to configure [HTTP.SYS](https://www.iis.net/learn/get-started/introduction-to-iis/introduction-to-iis-architecture#Hypertext) in Windows.
 
++ SFDevTest: An Azure Resource project for a secure Service Fabric cluster ARM template
+of a three node dev/test cluster with [Service Fabric Reverse Proxy](https://azure.microsoft.com/en-us/documentation/articles/service-fabric-reverseproxy/) 
+enabled for HTTPS on port 19008. To make things easy, simply use the cluster's management certificate also as the reverse proxy's
+HTTPS certificate.   
+
 ***
 
-In order to create your own self-signed certificate(s), follow these steps (this applies to both Kestrel and WebListener):
+In order to create your own self-signed certificate(s) on your development machine, follow these steps (this applies to both Kestrel
+ and WebListener):
 1. Create the certificate:  
 Launch PowerShell as local administrator, and execute  
  `New-SelfSignedCertificate -DnsName localhost -FriendlyName "Service Fabric Development Certificate" -CertStoreLocation "Cert:\LocalMachine\My\"`
---
+++ SFDevTest/Add-VmssCertificate.ps1
@@ -0,0 +1,58 @@
+﻿# Based on https://gist.github.com/andersosthus/c483eaf8630219c789de
+#
+# Use this script to add a certificate stored in KeyVault to a Service Fabric Cluster's
+# Virtual Machine Scale Set (or any stand-alone VMSS).  
+# This script requires an existing KeyVault that is EnabledForDeployment.  
+# The vault must be in the same region as the cluster.
+# 
+# To create a new vault and set the EnabledForDeployment property run:
+#
+# New-AzureRmResourceGroup -Name KeyVaults -Location WestUS
+# New-AzureRmKeyVault -VaultName $KeyVaultName -ResourceGroupName KeyVaults -Location WestUS -EnabledForDeployment
+#
+
+# Add a certificate to a Virtual Machine Scale Set.
+#
+# Note that the code in line 36 assumes the VMSS is using exactly one Key Vault
+# to store secrets (hence $Vmss.VirtualMachineProfile.OsProfile.Secrets[0]).
+
+Param
+(
+    [Parameter(Mandatory = $true)]
+    [string]
+    $SourceVault,
+
+    [Parameter(Mandatory = $true)]
+    [string]
+    $ResourceGroup,
+
+    [Parameter(Mandatory = $true)]
+    [string]
+    $CertificateUrl,
+
+    [Parameter(Mandatory = $true)]
+    [string]
+    $VmssName
+)
+
+$CertStore = "My"
+
+Write-Information "Getting Key Vault info for vault $SourceVault"
+$Vault = Get-AzureRmKeyVault -VaultName $SourceVault
+
+Write-Information "Getting VMSS info for VMSS $VmssName"
+$Vmss = Get-AzureRmVmss -ResourceGroupName $ResourceGroup -VMScaleSetName $VmssName
+
+Write-Information "Updating VMSS $VmssName"
+
+try
+{
+    $CertConfig = New-AzureRmVmssVaultCertificateConfig -CertificateUrl $CertificateUrl -CertificateStore $CertStore
+    $Vmss.VirtualMachineProfile.OsProfile.Secrets[0].VaultCertificates.Add($CertConfig) # NOTE: Change this as needed
+    Update-AzureRmVmss -ResourceGroupName $ResourceGroup -Name $VmssName -VirtualMachineScaleSet $Vmss
+}
+Catch
+{
+    $ExceptionMessage = $_.Exception.Message
+    Write-Warning $ExceptionMessage
+}
